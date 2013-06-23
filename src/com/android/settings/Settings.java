1d4ee4c72141dcf26bf13c0ac44496fc95573feb
TripNRaVeR <noerialexeew@live.nl>
2013-06-23 03:19:27 +0200

    Settings: Notification Shortcuts (2/2)

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -880,4 +880,5 @@ public class Settings extends PreferenceActivity
     public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
+    public static class NotificationShortcutsSettingsActivity extends Settings { /* empty */ }
 }
