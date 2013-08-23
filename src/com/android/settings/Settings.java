e6bd2503377719611470cd31bc6bd230bafff3a7
TripNRaVeR <noerialexeew@live.nl>
2013-08-23 02:00:17 +0200

    make accessibility settings a sub menu of language and input
    Conflicts:
    
    	AndroidManifest.xml
    	res/values/cm_strings.xml
    	res/xml/settings_headers.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -161,7 +161,6 @@ public class Settings extends PreferenceActivity
             R.id.system_section,
             R.id.date_time_settings,
             R.id.about_settings,
-            R.id.accessibility_settings,
             R.id.print_settings
     };
 
