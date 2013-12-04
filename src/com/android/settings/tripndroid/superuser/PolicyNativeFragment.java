edf17c695b7176eea804904e69ba9cad7b935d08
TripNRaVeR <noerialexeew@live.nl>
2013-12-04 02:31:04 +0100

    tripndroid superuser

--- src/com/android/settings/tripndroid/superuser/PolicyNativeFragment.java
+++ src/com/android/settings/tripndroid/superuser/PolicyNativeFragment.java
@@ -1,4 +1,4 @@
-package com.android.settings.cyanogenmod.superuser;
+package com.android.settings.tripndroid.superuser;
 
 import android.content.res.Resources;
 import android.os.Bundle;
--- src/com/android/settings/tripndroid/superuser/RequestActivity.java
+++ src/com/android/settings/tripndroid/superuser/RequestActivity.java
@@ -1,4 +1,4 @@
-package com.android.settings.cyanogenmod.superuser;
+package com.android.settings.tripndroid.superuser;
 
 public class RequestActivity extends com.koushikdutta.superuser.RequestActivity {
 }
