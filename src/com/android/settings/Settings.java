016d7e989d6ec87e8189d3fc5d05b8e16c3a0e86
Danny Baumann <dannybaumann@web.de>
2013-12-11 14:33:18 +0100

    Re-add system settings fragment direct link.
    Used by expanded desktop QS tile.
    
    Change-Id: I486bee5d5ae2dd9723cf698f2e8cc4e62be071b3

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1158,4 +1158,5 @@ public class Settings extends PreferenceActivity
     public static class ApnSettingsActivity extends Settings { /* empty */ }
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
+    public static class SystemSettingsActivity extends Settings { /* empty */ }
 }
